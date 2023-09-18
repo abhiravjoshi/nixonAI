46c265cf5b2e6b30c7fd18ecedac93bb8e6dbbef
abhiravjoshi <jabhirav@gmail.com>
2023-09-18 16:11:47 -0400

    training parameters cleaned up based off  analysis notebooks

---
+++ infer.py
@@ -0,0 +1,80 @@
+# source: https://github.com/coqui-ai/TTS/issues/2281 @padmalcom
+# purpose of this file is to store the inference and model loading logic so we don't have to train every time.
+
+from TTS.tts.configs.vits_config import VitsConfig
+from TTS.config import load_config
+from TTS.tts.models.vits import Vits
+from TTS.utils.synthesizer import Synthesizer
+
+import sys
+
+def infer(m, c):
+    config = load_config(c)
+    model = Vits(config)
+
+    config = load_config(c)
+    model = Vits.init_from_config(config)
+    model.load_checkpoint(config, m, eval=True)
+
+    synth = Synthesizer(m, c)
+
+    wav = synth.tts(
+	    "Ask not what your country can do for you. Ask what you can do for your country.",
+    )
+    
+    synth.save_wav(wav, "output.wav")
+    
+# python3 infer.py /path/to/model /path/to/config
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        model = sys.argv[1]
+        config = sys.argv[2]
+        infer(model, config)
+    else:
+        exit
+
+'''
+from TTS.api import TTS
+
+from TTS.tts.configs.vits_config import VitsConfig
+from TTS.tts.models.vits import Vits
+from TTS.config import load_config
+from TTS.utils.synthesizer import Synthesizer
+
+config = VitsConfig()
+model = Vits(config)
+
+config = load_config("config.json")
+model = Vits.init_from_config(config)
+model.load_checkpoint(config, 'best_model_226300.pth', eval=True)
+
+speakers_file_path = None
+language_ids_file_path = None
+vocoder_path = None
+vocoder_config_path = None
+encoder_path = None
+encoder_config_path = None
+cuda = True
+
+synthesizer = Synthesizer("best_model_226300.pth", "config.json", speakers_file_path, language_ids_file_path, vocoder_path, vocoder_config_path, encoder_path, encoder_config_path, cuda)
+
+speaker_idx = None
+language_idx = None
+speaker_wav = None
+reference_wav = None
+style_wav = None
+style_text = None
+reference_speaker_name = None
+wav = synthesizer.tts(
+	"Das ist ein einfacher Test.",
+	speaker_idx,
+	language_idx,
+	speaker_wav,
+	reference_wav,
+	style_wav,
+	style_text,
+	reference_speaker_name
+)
+synthesizer.save_wav(wav, "output.wav")
+'''
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # Abhirav Joshi (@abhiravjoshi)
 # Sumedh Garimella (@hdemusg)
 
+# Purpose of main.py: generate dataset and train Vits model
+
 import openai
 import sys
 import io
 import os
+from pprint import pprint
 
 from trainer import Trainer, TrainerArgs
 
@@ -15,6 +18,7 @@ from TTS.tts.models.vits import Vits, VitsArgs, VitsAudioConfig
 from TTS.tts.utils.speakers import SpeakerManager
 from TTS.tts.utils.text.tokenizer import TTSTokenizer
 from TTS.utils.audio import AudioProcessor
+from TTS.config.shared_configs import BaseAudioConfig
 
 API_KEY = None
 
@@ -48,9 +52,46 @@ def main():
     dataset_config = BaseDatasetConfig(
         formatter="ljspeech", meta_file_train="../transcript.txt", path=os.path.join(os.getcwd(), "dataset/training/"))
 
-    audio_config = VitsAudioConfig(
-        sample_rate=44100, win_length=1024, hop_length=256, num_mels=80, mel_fmin=0, mel_fmax=None
-    )
+    audio_config = BaseAudioConfig()
+
+    tune_params={
+        'num_mels': 80,          # In general, you don't need to change this. 
+        'fft_size': 2400,        # In general, you don't need to change this.
+        'frame_length_ms': 50, 
+        'frame_shift_ms': 12.5,
+        'sample_rate': 44100,    # This must match the sample rate of the dataset.
+        'hop_length': 256,       # In general, you don't need to change this.
+        'win_length': 1024,      # In general, you don't need to change this.
+        'preemphasis': 0.97,     # In general, 0 gives better voice recovery but makes training harder. If your model does not train, try 0.97 - 0.99.
+        'min_level_db': -100,
+        'ref_level_db': 5,       # The base DB; increase until all background noise is removed in the spectrogram, then lower until you hear better speech below.
+        'power': 1.5,            # Change this value and listen to the synthesized voice. 1.2 - 1.5 are resonable values.
+        'griffin_lim_iters': 60, # Quality does not improve for values > 60
+        'mel_fmin': 0.0,         # Adjust this and check mel-spectrogram-based voice synthesis below.
+        'mel_fmax': 0.85,      # Adjust this and check mel-spectrogram-based voice synthesis below.
+        'do_trim_silence': True  # If you dataset has some silience at the beginning or end, this trims it. Check the AP.load_wav() below,if it causes any difference for the loaded audio file.
+    }
+
+    # These options have to be forced off in order to avoid errors about the 
+    # pre-calculated not matching the options being tuned.
+    reset={
+        'signal_norm': True,  # check this if you want to test normalization parameters.
+        'stats_path': None,
+        'symmetric_norm': False,
+        'max_norm': 1,
+        'clip_norm': True,
+    }
+
+    # Override select parts of loaded config with parameters above
+    tuned_config = audio_config.copy()
+    tuned_config.update(reset)
+    tuned_config.update(tune_params)
+
+    
+
+    #audio_config = VitsAudioConfig(
+    #    sample_rate=44100, win_length=1024, hop_length=256, num_mels=80, mel_fmin=0, mel_fmax=8000.00, preemphasis=0.95, rel_level_db=5
+    #)
 
     character_config = CharactersConfig(
         characters_class= "TTS.tts.models.vits.VitsCharacters",
@@ -68,11 +109,12 @@ def main():
         run_name="vits_vctk",
         batch_size=16,
         eval_batch_size=4,
+        eval_split_size=0.25,
         num_loader_workers=4,
         num_eval_loader_workers=4,
         run_eval=True,
         test_delay_epochs=0,
-        epochs=50,
+        epochs=10,
         text_cleaner="basic_cleaners",
         use_phonemes=False,
         phoneme_language="en-us",
@@ -80,7 +122,7 @@ def main():
         compute_input_seq_cache=True,
         print_step=25,
         print_eval=False,
-        save_best_after=1000,
+        save_best_after=10,
         save_checkpoints=True,
         save_all_best=True,
         mixed_precision=False,
@@ -90,24 +132,26 @@ def main():
         cudnn_benchmark=False,
         test_sentences=[
             ["These folks are good people... they deserve the same as the rest of us."],
-            ["I'm a fighter, Pat. And I'll always be one."],
-            ["Helen Gahagan Douglas!? She's pink all the way down to her underwear!"]
-        ]
+            ["I'm a fighter, Pat. And I'll always be one."]
+        ],
+        
     )
 
     # Audio processor is used for feature extraction and audio I/O.
     # It mainly serves to the dataloader and the training loggers.
-    ap = AudioProcessor.init_from_config(config)
+    ap = AudioProcessor(**tuned_config)
 
     # INITIALIZE THE TOKENIZER
     # Tokenizer is used to convert text to sequences of token IDs.
     # config is updated with the default characters if not defined in the config.
     tokenizer, config = TTSTokenizer.init_from_config(config)
 
-    train_samples, eval_samples = load_tts_samples( dataset_config, 
+    train_samples, eval_samples = load_tts_samples(dataset_config, 
                                                     eval_split=True, 
+                                                    eval_split_size=config.eval_split_size,
                                                     formatter=formatter)
         
+    print(len(train_samples), len(eval_samples))
 
     # init model
     model = Vits(config, ap, tokenizer, speaker_manager=None)
@@ -129,15 +173,15 @@ def formatter(root_path, manifest_file, **kwargs):  # pylint: disable=unused-arg
     """
     txt_file = os.path.join(root_path, manifest_file)
     items = []
-    speaker_name = "Richard Milhous Nixon"
+    speaker_name = "RN"
     with open(txt_file, "r", encoding="utf-8") as ttf:
         for line in ttf:
             cols = line.split("|")
             filename = "dataset/training/" + cols[0] + ".wav"
             wav_file = os.path.join(os.getcwd(), filename)
-            text = cols[1]
+            text = cols[1].replace('\n', '')
             # print(text)
-            items.append({"text":text, "audio_file":wav_file, "speaker_name":speaker_name, "root_path": root_path})
+            items.append({"text":text, "audio_file":wav_file, "speaker_name":speaker_name, "root_path": root_path, "language": "eng"})
     return items
 
 
